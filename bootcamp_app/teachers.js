9e272ecc133c293c8a98f12f069bddb70dcddf59
Luana Duarte <lua.duarte04@gmail.com>
2020-04-02 00:53:35 +0000

    add teacher.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,39 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+});
+
+
+// pool.query(`
+// SELECT cohorts.name as cohort, teachers.name as teachers
+// FROM teachers
+// JOIN assistance_requests ON assistance_requests.student_id = students.id
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.cohort}: ${user.name}`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
