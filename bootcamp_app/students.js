5ae0c1e3f6946a05fc1a075c8ecbe6fef4d9e7bc
Luana Duarte <lua.duarte04@gmail.com>
2020-04-02 17:28:34 +0000

    refactoring code for students and ateachers js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,20 +7,6 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// SHOULD I COMMENT THAT? OR JUST UPDATE THE VALUES?
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
-
 // for this
 const cohortName = process.argv[2];
 const limit = process.argv[3] || 5;
@@ -35,7 +21,28 @@ const queryString = `
   LIMIT $2;
 `;
 
-pool.query(queryString, values);
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  }).catch(err => console.error('query error', err.stack));
+
+
+// SHOULD I COMMENT THAT? OR JUST UPDATE THE VALUES?
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// }).catch(err => console.error('query error', err.stack));
+
 
 
 // pool.query(`
